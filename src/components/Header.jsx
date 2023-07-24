62a3cbe344e04a5106c26d4fff9acf189edb5851
Lucas Muller <lucas.mullerqdv@gmail.com>
2023-07-24 11:28:10 -0300

    Implemented Searchbar and Hamburguer menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import logo from '../img/logo.png';
 import "../css/Header.css";
 
@@ -25,12 +25,32 @@ class Header extends React.Component {
             />
           </Navbar.Brand>
           <div className="d-flex flex-grow-1 justify-content-center">
-          <form class="d-flex" action="javascript:getData()">
-            <input id="pesqtxt" class="form-control me-2" placeholder="Buscar Cidade"></input>
-            <button id="pesqbtn" class="btn btn-outline-success" type='button'>Pesquisar</button>
+          <form class="form">
+              <button>
+                  <svg width="17" height="16" fill="none" xmlns="http://www.w3.org/2000/svg" role="img" aria-labelledby="search">
+                      <path d="M7.667 12.667A5.333 5.333 0 107.667 2a5.333 5.333 0 000 10.667zM14.334 14l-2.9-2.9" stroke="currentColor" stroke-width="1.333" stroke-linecap="round" stroke-linejoin="round"></path>
+                  </svg>
+              </button>
+              <input class="input" placeholder="Pesquisar" required="" type="text"></input>
+              <button class="reset" type="reset">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path>
+                  </svg>
+              </button>
           </form>
           </div>
         </Container>
+        <Nav>
+        <button id="btnHeader">
+        <svg width="20" height="20" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 18L20 18" stroke="#000000" stroke-width="2" stroke-linecap="round"></path> <path d="M4 12L20 12" stroke="#000000" stroke-width="2" stroke-linecap="round"></path> <path d="M4 6L20 6" stroke="#000000" stroke-width="2" stroke-linecap="round"></path> </g></svg>
+                  
+              </button>
+              <button class="reset" type="reset">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path>
+                  </svg>
+              </button>
+          </Nav>
       </Navbar>
       );
   }
